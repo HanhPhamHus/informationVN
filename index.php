8483722b98dd38f083c9e571c6b40745713d38b1
HanhPham <44548605+HanhPhamHus@users.noreply.github.com>
2019-12-10 17:51:01 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,181 @@
+<?php
+ob_start();
+session_start();//biến tồn trại trong suốt quá trình di chuyển giữa các trang web 
+require "lib/dbCon.php";
+require "lib/trangchu.php";//khai báo hàm để dùng
+if(isset($_GET["p"]))
+    $p= $_GET["p"];
+else {
+    $p = "";
+}
+?>
+<?php
+//kiểm tra loginư
+
+    if(isset($_POST["btnLogin"])){
+        $un=$_POST["txtUn"];
+        $pa=$_POST["txtPa"];
+        $pa=md5( $pa);
+        $con= mysqli_connect("localhost","root","","informationvn");
+        $qr = "
+            SELECT *  FROM users
+            WHERE Username= '$un'
+            AND Password ='$pa'
+        ";
+       
+        $user = mysqli_query($con,$qr);
+        if(mysqli_num_rows($user)==1){
+            $row=mysqli_fetch_array($user);
+            $_SESSION["idUser"]=$row['idUser'];
+            $_SESSION["Username"]=$row['Username'];  
+            $_SESSION["HoTen"]=$row['HoTen'];
+            $_SESSION["idGroup"]=$row['idGroup'];
+        }
+    }
+?>
+<?php
+//thoát
+    if(isset($_POST["btnThoat"])){
+        unset($_SESSION["idUser"]);
+        unset($_SESSION["Username"]);
+        unset($_SESSION["HoTen"]);
+        unset($_SESSION["idGroup"]);
+    }
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Informations VietNam</title>
+<link rel="stylesheet" type="text/css" href="css/layout.css" />
+</head>
+
+<body>
+<div id="wrap-vp">
+	<div id="header-vp">
+    	<div id="logo"><img style="width:204px;height:59px" src="images/logo2.jpg" /></div>
+    </div>
+    
+    <div>
+        <!--block/menu.php-->
+        <?php
+            require "blocks/menu.php";
+        ?>
+    </div>
+    <div id="midheader-vp">
+    	<div id="left">
+        	<ul class="list_arrow_breakumb">
+            	<li class="start">
+                <a href="javascript:;">Trang nhất</a>
+                <span class="arrow_breakumb">&nbsp;</span></li>
+           </ul>
+        </div>
+        <div id="right">
+            <!--blocks/thongtinchung.php-->
+            <?php
+                require "blocks/thongtinchung.php";
+            ?>
+        </div>
+    </div>
+    <div class="clear"></div>
+
+    <div id="slide-vp">
+        <!--blocks/top_trang_chu.php-->
+        <?php
+            require "blocks/top_trang_chu.php";
+        ?>
+
+        <div id="slide-right">
+        <?php
+            require "blocks/quangcao_top_phai.php";
+        ?>
+        <!--blocks/quangcao_top_phai.php-->
+        </div>
+    </div>
+
+  	<div id="content-vp">
+    	<div id="content-left">
+        <!--blocks/cot_trai.php-->
+        <?php
+            require "blocks/cot_trai.php";
+        ?>
+        </div>
+        <div id="content-main">
+			
+            <!--PAGES-->
+
+            <?php
+                switch($p){
+                    case "tintrongloai" :  
+                        require "pages/tintrongloai.php";
+                        // header("./tintrongloai.php");
+                         break;
+                    case 'chitiettin':
+                        require "pages/chitiettin.php";
+                        break;
+                    case 'timkiem':
+                        require "pages/timkiem.php";
+                    break;
+                    default          :
+                        require "pages/trangchu.php";
+                }
+            
+            ?>
+        
+            
+        </div>
+        <div id="content-right">
+        <!--blocks/cot_phai.php-->
+        <?php 
+            if(!isset($_SESSION["idUser"])){
+                require "blocks/formLogin.php";
+            }
+            else{
+                require "blocks/formHello.php";
+            }
+        ?>
+        <?php
+            require "blocks/cot_phai.php";
+        ?>
+        </div>
+
+    <div class="clear"></div>
+    	
+    </div>
+    
+     <div id="thongtin">
+        <!--blocks/thongtindoanhnghiep.php-->
+        
+        <?php
+            require "blocks/thongtindoanhnghiep.php";
+        ?>
+    </div>
+    <div class="clear"></div>
+    <div id="footer">
+    	<!--blocks/footer.php-->
+        <?php
+            require "blocks/footer.php";
+        ?>
+        <div class="ft-bot">
+            <div class="bot1"><img src="images/logo2.jpg" /></div>
+            <div class="bot2">
+                     <p>© <span>Copyright 1997 InformationVn,</span>  All rights reserved</p>
+                     <p>® InformationVn giữ bản quyền nội dung trên website này.</p>
+            </div>
+            <div class="bot3">
+                
+                     <p><a href="#">InformationVn tuyển dụng</a>   <a href="http://polyad.net/Polyad/Lien-he.htm">Liên hệ quảng cáo</a> / <a href="/contactus">Liên hệ Tòa soạn</a></p>
+                     <p><a href="#" target="_blank" style="color: #686E7A;font: 11px arial;padding: 0 4px;text-decoration: none;">Thông tin Tòa soạn: </a><span>0123.888.0123</span> (HN) - <span>0129.233.3555</span> (TP HCM)</p>
+                  
+            </div>
+        </div>
+    </div>
+    
+    
+    
+    
+</div>
+
+</body>
+</html>
---
+++ loaitin.php
@@ -0,0 +1,17 @@
+<?php 
+    require "lib/dbCon.php";
+    require "lib/trangchu.php";
+?>
+<?php 
+    $idTL=$_GET["idTL"];
+    settype($idTL,"int");
+    $loaitin=DanhSachLoaiTin($idTL);
+    while($row_loaitin=mysqli_fetch_array($loaitin))
+    {
+
+?>   
+
+<option value="<?php echo $row_loaitin["idLT"]?>"><?php echo $row_loaitin["Ten"]?></option>
+<?php
+    }
+?>
